da5b8c5ddd1ee8c5a497669773f473d6bb4fe04a
alextenney <alexandra.tenney@ucalgary.ca>
2020-10-26 01:01:39 -0600

    trying to get the video running

--- CTFd-master/CTFd/views.py
+++ CTFd-master/CTFd/views.py
@@ -143,9 +143,9 @@ def setup():
 
             # Index page
 
-            index = """<div class="row">
+            index = <div class="row">
     <div class="col-md-6 offset-md-3">
-        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
+        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.mp4" />
         <h3 class="text-center">
             <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
             <p>Follow us on social media:</p>
@@ -158,7 +158,7 @@ def setup():
             <a href="admin">Click here</a> to login and setup your CTF
         </h4>
     </div>
-</div>"""
+</div>
 
             page = Pages(title=None, route="index", content=index, draft=False)
 
