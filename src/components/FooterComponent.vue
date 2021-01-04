f08907cca1c06748571a358213734d3e950b891a
James Lowther <jamesevlowther@gmail.com>
2021-01-03 19:28:53 -0700

    Added about information

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-16">
     <div id="" class="pb-6 w-full text-center inline-block align-middle text-white text-xl">
-      <span>magpieCTF is hosted by the <a target=”_blank” href="https://infosecucalgary.ca" class="no-underline hover:underline text-pink-700">Information Security Club</a> from the University of Calgary - &copy;2020</span>
+      <span>magpieCTF is hosted by the <a target=”_blank” href="https://infosecucalgary.ca" class="no-underline hover:underline text-pink-700">Information Security Club</a> from the University of Calgary - &copy;2021</span>
     </div>
   </div>
 </template>
