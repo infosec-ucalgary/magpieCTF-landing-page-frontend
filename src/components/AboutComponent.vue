74b99673c35892bb5404d8a48498dadbc88fa278
James Lowther <jamesevlowther@gmail.com>
2021-01-03 19:37:47 -0700

    Fixed text size

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,40 +1,40 @@
 <template>
   <section id="about">
     <div class="mx-auto w-2/3">
-      <h1 class="text-5xl text-center mt-5 mb-3 neon-red">About</h1>
+      <p class="text-5xl text-center mt-5 mb-3 neon-red">About</p>
       <div>
         <div class="mb-5">
-          <h3 class="neon-blue text-left mb-4">What is it?</h3>
-          <h5 class="text-white mb-2">If you have no idea what a CTF is, <a href="https://www.youtube.com/watch?v=8ev9ZX9J45A" target="_blank"><u>this video</u></a> should help. magpieCTF is a jeopardy style, team based, totally online CTF taking place over two days with thousands of dollars in cash prizes and subscriptions. This is a beginner to intermediate level CTF, so if you're brand new to CTFs/cybersecurity/hacking this is your chance to get started!</h5>
-        <h5 class="text-white">Our first annual CTF will be 80s themed (culture, movies, music, technology). Calgary tech companies are watching (some are participating!) and they will have space online to talk to you during the competition as your test your skills and show them what you can do!</h5>
+          <p class="text-3xl neon-blue text-left mb-4">What is it?</p>
+          <p class="text-xl text-white mb-2">If you have no idea what a CTF is, <a href="https://www.youtube.com/watch?v=8ev9ZX9J45A" target="_blank"><u>this video</u></a> should help. magpieCTF is a jeopardy style, team based, totally online CTF taking place over two days with thousands of dollars in cash prizes and subscriptions. This is a beginner to intermediate level CTF, so if you're brand new to CTFs/cybersecurity/hacking this is your chance to get started!</p>
+          <p class="text-xl text-white">Our first annual CTF will be 80s themed (culture, movies, music, technology). Calgary tech companies are watching (some are participating!) and they will have space online to talk to you during the competition as your test your skills and show them what you can do!</p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-green text-left mb-4">When is it?</h3>
-          <h5 class="text-white">magpieCTF is a two day event starting <strong>February 19th at 4pm and ending February 21st at 4pm.</strong></h5>
+          <p class="text-3xl neon-green text-left mb-4">When is it?</p>
+          <p class="text-xl text-white">magpieCTF is a two day event starting <strong>February 19th at 4pm and ending February 21st at 4pm.</strong></p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-yellow text-left mb-4">What does it cost?</h3>
-          <h5 class="text-white">There is no entry fee, it is totally free to enter!</h5>
+          <p class="text-3xl neon-yellow text-left mb-4">What does it cost?</p>
+          <p class="text-xl text-white">There is no entry fee, it is totally free to enter!</p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-red text-left mb-4">What are the prizes?</h3>
-          <h5 class="text-white">First place is <strong>$1024</strong>, second is <strong>$512</strong>, third is <strong>$256</strong>. There are also subscriptions to services and swag in the mix!</h5>
+          <p class="text-3xl neon-red text-left mb-4">What are the prizes?</p>
+          <p class="text-xl text-white">First place is <strong>$1024</strong>, second is <strong>$512</strong>, third is <strong>$256</strong>. There are also subscriptions to services and swag in the mix!</p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-blue text-left mb-4">What else do I need to know?</h3>
-          <h5 class="text-white">Prizes are limited to high school and university students in Western Canada. CTF is a team sport, and the maximum team size is 5. You don't have to be on a team but it REALLY helps and is more fun. We've limited the number of tickets so sign up as soon as you can. You register as an individual and then created/join a team later.</h5>
+          <p class="text-3xl neon-blue text-left mb-4">What else do I need to know?</p>
+          <p class="text-xl text-white">Prizes are limited to high school and university students in Western Canada. CTF is a team sport, and the maximum team size is 5. You don't have to be on a team but it REALLY helps and is more fun. We've limited the number of tickets so sign up as soon as you can. You register as an individual and then created/join a team later.</p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-green text-left mb-4">Where do I sign up?</h3>
-          <h5 class="text-white">Start by signing up for a ticket here on our <a href="https://www.eventbrite.ca/e/magpiectf-tickets-128583228899" target="_blank"><u>Eventbrite page</u></a>. We'll email you back with confirmation of your registration and details for your login on our CTF site. Once there, you can make your team. The CTFd page for the competition is <a href="https://2021.magpiectf.ca/" target="_blank"><u>here</u></a>.</h5>
+          <p class="text-3xl neon-green text-left mb-4">Where do I sign up?</p>
+          <p class="text-xl text-white">Start by signing up for a ticket here on our <a href="https://www.eventbrite.ca/e/magpiectf-tickets-128583228899" target="_blank"><u>Eventbrite page</u></a>. We'll email you back with confirmation of your registration and details for your login on our CTF site. Once there, you can make your team. The CTFd page for the competition is <a href="https://2021.magpiectf.ca/" target="_blank"><u>here</u></a>.</p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-yellow text-left mb-4">What if I'm not a student?</h3>
-          <h5 class="text-white">If cybersecurity if your hobby, career, or your business and you want to get involved please drop us a line and let us know! We're hoping to build the community and give students the chance to interact with you. This is your opportunity to meet with future talent, connect, and recruit.</h5>
+          <p class="text-3xl neon-yellow text-left mb-4">What if I'm not a student?</p>
+          <p class="text-xl text-white">If cybersecurity if your hobby, career, or your business and you want to get involved please drop us a line and let us know! We're hoping to build the community and give students the chance to interact with you. This is your opportunity to meet with future talent, connect, and recruit.</p>
         </div>
         <div class="mb-5">
-          <h3 class="neon-red text-left mb-4">What if I still have questions?</h3>
-          <h5 class="text-white">The best place to ask is the <a href="https://discord.gg/QByARV237Q" target="_blank"><u>magpieCTF Discord server</u></a>. You can private message us there, or <a href="mailto:infosec.ucalgary@gmail.com" target="_blank"><u>email us</u></a>.</h5>
+          <p class="text-3xl neon-red text-left mb-4">What if I still have questions?</p>
+          <p class="text-xl text-white">The best place to ask is the <a href="https://discord.gg/QByARV237Q" target="_blank"><u>magpieCTF Discord server</u></a>. You can private message us there, or <a href="mailto:infosec.ucalgary@gmail.com" target="_blank"><u>email us</u></a>.</p>
         </div>
       </div>
     </div>
