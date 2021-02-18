53351f12c852ddaf23708e36dab6e0b5a72ffc21
James Lowther <jamesevlowther@gmail.com>
2021-02-17 19:56:10 -0700

    Added Cisco and SecuredNet

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -10,7 +10,7 @@
       <div class="flex flex-col">
         <section class="w-full">
           <TimerComponent />
-          <p class="font-semibold text-center text-6xl my-32">
+          <p class="font-semibold text-center text-3xl sm:text-6xl my-32">
             <span class="neon-red">19/02/2021 4PM MST</span>
             <span class="neon-blue"> - </span>
             <span class="neon-green">21/02/2021 4PM MST</span>
@@ -28,13 +28,19 @@
           <a href="https://discord.gg/wVwv5Pw"><img class="mx-auto" src="@/assets/Discord-Logo+Wordmark-Color.png" style="width: 45%" /></a>
         </div>
         -->
-        <div class="w-2/3 mx-auto my-16 pb-6 pt-2 border-4 rounded-lg neon-border">
-          <p class="text-center font-display text-5xl text-white mb-16">Sponsored By</p>
+        <div class="w-5/6 sm:w-2/3 mx-auto my-16 pb-6 pt-2 border-4 rounded-lg neon-border overflow-x-auto">
+          <p class="text-center font-display text-3xl sm:text-5xl text-white mb-16">Sponsored By</p>
           <div class="my-6 mx-6">
-            <img src="@/assets/HTB-Logo-RGB_1024.svg" style="min-width: 200px; max-width: 500px;" class="mx-auto"/>
+            <img src="@/assets/HTB-Logo-RGB_1024.svg" style="min-width: 200px; max-width: 550px;" class="mx-auto"/>
           </div>
           <div class="my-6 mx-6">
             <img src="@/assets/HSBC_MASTERBRAND_BANK_RGB_MONO_REV.svg" style="min-width: 200px; max-width: 500px;" class="mx-auto"/>
+          </div>
+          <div class="mb-8 mx-6">
+            <img src="@/assets/cisco-logo.png" style="max-height:120px; " class="mx-auto"/>
+          </div>
+          <div class="mb-10 my-24 mx-6">
+            <img src="@/assets/securednet.png" style="min-width: 200px;" class="mx-auto"/>
           </div>
         </div>
         <AboutComponent />
