4a8c9abf729cb0a481282a48aad9c165cc68880d
Nicholasm24 <nicholasmackinnon9@gmail.com>
2024-01-30 23:35:08 -0700

    created grid layout of images

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="relative w-full h-screen bg-black bg-cover bg-right sm:bg-left-bottom"
-    :style="{ backgroundImage: `url(${bgImage})` }" id="outer-container">
-
+  <div class="w-full h-screen bg-cover bg-left-bottom" :style="{ backgroundImage: `url(${bgImage})` }"
+    id="outer-container">
+    <div class="max-w-screen-xl mx-auto">
+      <div class="inline-grid grid-cols-3 gap-4">
+        <div class="w-full flex justify-center items-center">
+          <img class="w-1/2 h-auto" src="../assets/assets24/AberrantWatchdogs.webp">
+        </div>
+        <div class="w-full flex justify-center items-center">
+          <img class="w-1/2 h-auto" src="../assets/assets24/BinCortex.webp">
+        </div>
+        <div class="w-full flex justify-center items-center">
+          <img class="w-1/2 h-auto" src="../assets/assets24/QuantumLocks.webp">
+        </div>
+        <div class="w-full flex justify-center items-center">
+          <img class="w-1/2 h-auto" src="../assets/assets24/REAssembly.webp">
+        </div>
+        <div class="w-full flex justify-center items-center">
+          <img class="w-1/2 h-auto" src="../assets/assets24/Rogueware.webp">
+        </div>
+        <div class="w-full flex justify-center items-center">
+          <img class="w-1/2 h-auto" src="../assets/assets24/Singularity.webp">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
