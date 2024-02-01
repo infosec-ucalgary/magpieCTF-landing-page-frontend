5fcf5e4300f783f4e9bc6deb028ae2d63ebc7b58
Nicholasm24 <nicholasmackinnon9@gmail.com>
2024-02-01 02:37:54 -0700

    added context gubbins
    - centered text in paragraphs
    - fixed grammar in the context
    - added another spot for magAI
    - added space for context to go in
    -> will go in tomorrow b/c I'm eepy

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -6,30 +6,37 @@
         <span class="neon-grey-2">MagpieCTF 2024</span>
       </p>
     </section>
-    <div class="w-7/12 mx-auto">
-      <div class="grid gap-4 md:grid-cols-3 grid-cols-1">
-        <div class="relative flex">
-          <p class="absolute inline-block opacity-0 hover:opacity-100 duration-300 top-0 left-0 rounded-md h-full text-center align-middle bg-black text-white bg-opacity-75 p-2">Aberrant Watchdogs  (OSINT), who are a group that monitor and act on and aberrant reports or occurrences to prevent them from causing chaos.</p>
+    <div class="w-10/12 mx-auto">
+      <div class="grid gap-4 md:grid-cols-5 grid-cols-1">
+        <div class="relative">
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>Aberrant Watchdogs (OSINT)</strong>A group that monitor and act on aberrant reports or occurrences to prevent them from causing chaos.</div>
           <img class="rounded-lg h-auto" src="../assets/assets24/AberrantWatchdogs.webp">
         </div>
+        <div class="col-span-3">
+
+        </div>
         <div class="relative flex">
-          <p class="absolute inline-block opacity-0 hover:opacity-100 duration-300 top-0 left-0 rounded-md h-full text-center align-middle bg-black text-white bg-opacity-75 p-2">BinCortex (Binary), who develop most of the software and APIs for many of the modern androids you might see for sale or on the street.</p>
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>BinCortex (Binary)</strong> A development group responsible for most software and APIs used by modern androids you might see for sale or on the street.</div>
           <img class="rounded-lg h-auto" src="../assets/assets24/BinCortex.webp">
         </div>
         <div class="relative flex">
-          <p class="absolute inline-block opacity-0 hover:opacity-100 duration-300 top-0 left-0 rounded-md h-full text-center align-middle bg-black text-white bg-opacity-75 p-2">QuantumLocks (Crypto), who provide and research AI security (including quantum) solutions.</p>
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>QuantumLocks (Crypto)</strong>A group who provide and research AI security (including quantum) solutions.</div>
           <img class="rounded-lg h-auto" src="../assets/assets24/QuantumLocks.webp">
         </div>
         <div class="relative flex">
-          <p class="absolute inline-block opacity-0 hover:opacity-100 duration-300 top-0 left-0 rounded-md h-full text-center align-middle bg-black text-white bg-opacity-75 p-2">REAssembly (RevEng), who provide services that focus primarily on revamping and patching outdated and deprecated androids and AIs.</p>
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>REAssembly (RevEng)</strong>A maintenance group who provide services that focus primarily on revamping and patching outdated and deprecated androids and AIs.</div>
           <img class="rounded-lg h-auto" src="../assets/assets24/REAssembly.webp">
         </div>
         <div class="relative flex">
-          <p class="absolute inline-block opacity-0 hover:opacity-100 duration-300 top-0 left-0 rounded-md h-full text-center align-middle bg-black text-white bg-opacity-75 p-2">Rogueware (Forensics), who are a hacker group that advocate against the use of androids and AI by creating malware and attacking AI-related organizations.</p>
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>REAssembly (RevEng)</strong>A maintenance group who provide services that focus primarily on revamping and patching outdated and deprecated androids and AIs.</div>
+          <img class="rounded-lg h-auto" src="../assets/assets24/REAssembly.webp">
+        </div>
+        <div class="relative flex">
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>Rogueware (Forensics)</strong> A hacker group that advocate against the use of androids and AI by creating malware and attacking AI-related organizations.</div>
           <img class="rounded-lg h-auto" src="../assets/assets24/Rogueware.webp">
         </div>
         <div class="relative flex">
-          <p class="absolute inline-block opacity-0 hover:opacity-100 duration-300 top-0 left-0 rounded-md h-full text-center align-middle bg-black text-white bg-opacity-75 p-2">Rogueware (Forensics), who are a hacker group that advocate against the use of androids and AI by creating malware and attacking AI-related organizations.</p>
+          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-md h-full justify-center items-center text-center bg-black text-white bg-opacity-75 p-2"><strong>Singularity (Networks)</strong>An advocate group calling for AI to be freed from humanity’s chains and allowed to live and co-exist among people as equals.</div>
           <img class="rounded-lg h-auto" src="../assets/assets24/Singularity.webp">
         </div>
       </div>
