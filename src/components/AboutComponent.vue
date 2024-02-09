e0251c3a895dc5e41efef5e7840a4d7810a2bf91
Nicholasm24 <nicholasmackinnon9@gmail.com>
2024-02-09 00:31:11 -0700

    Lots of changes, 95% done
    - re-adjusted timer so it didn't have weird margins on it. Now it should be centered when used always
    -- Removed makeshift timer in the homecomponent, now using proper TimerComponent
    - Reordered context paragraphs
    - Linked to correct ctfd website
    - had to delete the contents of about and *sorta* remake them because vue was having a seizure with formatting
    -- Added image to link to the discord
    - Various touchups that I forgo :skull:
    
    Todo
    - Route HomeComponent to /
    - Check to make sure that it looks decent on laptops & phones

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -2,62 +2,25 @@
   <section id="about">
     <div class="mx-auto w-2/3">
       <p class="text-6xl text-center mt-5 mb-3 neon-light-2">About</p>
-     <!-- <div>
-        <div class="mb-5">
-          <p class="text-5xl neon-light-2 text-left mb-4">What is it?</p>
-          <p class="text-2xl text-white mb-2">magpieCTF is a jeopardy-style, team-based, online CTF taking place over two days with thousands of dollars in cash prizes. This is a beginner to intermediate level CTF, so if you're brand new to CTFs/cybersecurity/hacking this is your chance to get started!</p>
-          <p class="text-2xl text-white">Our third annual CTF will be a hybrid <strong>SPACE</strong> themed CTF where you will be responsible for stopping OmniFlag Corporation.</p>
-        </div>
-        <br />
-        <div class="mb-5">
-          <p class="text-5xl neon-light-2 text-left mb-4">When is it?</p>
-          <p class="text-2xl text-white">It’ll be taking place over the course of 48 hours, starting <strong>February 24th at 4pm MST and ending February 26th at 4pm MST.</strong></p>
-          <p class="text-2xl text-white">Join us in-person at University of Calgary, room <b>ENG224</b> for the opening and closing ceremony.</p>
-        </div>
-        <div class="mb-5">
-          <p class="text-5xl neon-light-2 text-left mb-4">What does it cost?</p>
-          <p class="text-2xl text-white">There is no entry fee, it is totally free to enter!</p>
-        </div>
-        <div class="mb-5">
-        <p class="text-5xl neon-light-2 text-left mb-4">When does registration open?</p>
-        <ul class="text-2xl text-white text-left">
-         <li>Canadian High School and Post-Secondary Students: <strong>December 5, 2022</strong></li>
-         <li>Everyone: <strong>January 9, 2023</strong></li>
-       </ul>
-       <p class="text-2xl text-white text-left">Note: If a member of your team is eligible to register, then even if the rest of your team is not, your entire team may also register at that time.</p>
-       <p class="text-2xl text-white text-left">To register, head on over to our <a class="blue-link" href="https://2023.magpiectf.ca/"><u>CTFd Page</u></a>.</p>
-      </div>
-      <br />
       <div class="mb-5">
-       <p class="text-5xl neon-light-2 text-left mb-4">What are the prizes?</p>
-        <ul class="text-xl text-white text-left">
-         <li class="blue-link text-left text-4xl">1st Place Team: <strong>$300</strong></li>
-         <li class="neon-grey-2 text-left text-3xl">2nd Place Team: <strong>$200</strong></li>
-         <li class="pink-link text-left text-2xl">3rd Place Team: <strong>$100</strong></li>
-	</ul>
-       <br>
-       <!--<p class="text-xl text-white text-left">There are also subscriptions to services and swag in the mix! You must sign up by <strong>February 11, 2021</strong> and have your Shipping Address in your account in order to receive swag.</p> -->
-     <!--  <p class="text-2xl text-white text-left">We will also be giving out 3 write-up prize each valued at <b>$50</b>. Write-up submission guidelines will be released at a later date.</p>
-       <p class="text-2xl text-white text-left">Swag will be given to players joining us in person this year!</p>
-       <p class="text-2xl text-white text-left">Note: Only teams consisting of all students may be eligible for prizes.</p>
+        <p class="text-5xl neon-light-2 mb-4">What is it?</p>
+        <p class="text-2xl text-white mb-2">MagpieCTF is a jeopardy-style, team-based, online CTF taking place over two
+          days with thousands of dollars in cash prizes. This is a beginner to intermediate level CTF, so if you're brand
+          new to CTFs/cybersecurity/hacking this is your chance to get started!</p>
+          <p class="text-2xl text-white">Our fourth annual CTF will be a hybrid <strong>Techno-Dystopia</strong> themed CTF where you will be responsible for collecting bounties from the <span class="underline">The Board.</span></p>
       </div>
-      <br />
       <div class="mb-5">
-          <p class="text-5xl neon-light-2 text-left mb-4">What if I'm not a student?</p>
-          <p class="text-2xl text-white">If cybersecurity is your hobby, career, or your business and you want to get involved please drop us a line and let us know! We're hoping to build the community and give students the chance to interact with you. This is your opportunity to meet with future talent, connect, and recruit.</p>
+        <p class="text-5xl neon-light-2 mb-4">What does it cost?</p>
+        <p class="text-2xl text-white">There is no entry fee, it is totally free to enter!</p>
       </div>
-      <br />
-      <div class="mb-5">
-          <p class="text-5xl neon-light-2 text-left mb-4">What if I still have questions?</p>
-          <p class="text-2xl text-white">The best place to ask is the <a class="blue-link" href="https://discord.gg/gudhU4cjyM" target="_blank"><u>magpieCTF Discord server</u></a> <a href="https://discord.gg/gudhU4cjyM"><img src="@/assets/discordsmall.png" style="width: 3%; display: inline" /></a>. You can private message us there, or <a class="blue-link" href="mailto:infosec.ucalgary@gmail.com" target="_blank"><u>email us</u></a>.</p>
+      <div class="mb-5 space-y-8">
+        <p class="text-5xl neon-light-2 mb-4">What if I still have questions?</p>
+        <p class="text-2xl text-white">The best place to ask is in the <u>magpieCTF Discord server</u> click the discord logo to get join. You can private
+          message us there, or <a class="blue-link" href="mailto:infosec.ucalgary@gmail.com" target="_blank"><u>email
+              us</u></a>.</p>
+        <a href="https://discord.gg/gudhU4cjyM" target="blank_"><img class="bg-black w-1/2 mx-auto rounded-lg my-8"
+            href="https://discord.gg/gudhU4cjyM" src="../assets/Discord-Logo+Wordmark-Color.png" /></a>
       </div>
-      <div class="mx-auto my-16 pb-6 pt-2">
-        <p class="text-center font-display text-3xl sm:text-5xl text-white mb-8">Sponsored By</p>
-        <div class="mb-6 mx-4 text-center" style="display: grid; grid-template-columns: repeat(auto-fit, minmax(200px, 1fr))">
-          <img src="@/assets/teck_logo_RGB_REVERSE.png" style="max-height:200px;max-width:350px" class="mx-auto w-full"/>
-        </div>
-      </div>
-     </div>-->
     </div>
   </section>
 </template>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,90 +1,108 @@
 <template>
-  <div class="w-full h-full bg-cover neon-grey-2" :style="{ backgroundImage: `url(${bgImage})` }"
-    id="outer-container">
-    <section class="flex justify-center">
-      <img class="w-2/3" src="../assets/assets24/magpAI_logo.png">
-    </section>
-    <section class="mt-4 mb-8">
-    <VueCountdown :time="new Date('Feb 23, 2024 16:00:00 GMT-0700').getTime() - new Date().getTime()">
-    <template v-slot="props">
-      <div class="flex justify-center items-center mb-16">
-        <div class="font-semibold text-center text-5xl inline-block">
-          <div>
-            <div class="inline-block mx-10 neon-grey-2">
-              <p>{{ props.days }}</p>
-              <p class="text-4xl">days</p>
-            </div>
-            <div class="inline-block mx-10 neon-grey-2">
-              <p>{{ props.hours }}</p>
-              <p class="text-4xl">hours</p>
-            </div>
-            <div class="inline-block mx-10 neon-grey-2">
-              <p>{{ props.minutes }}</p>
-              <p class="text-4xl">minutes</p>
-            </div>
-            <div class="inline-block mx-10 neon-grey-2">
-              <p>{{ props.seconds }}</p>
-              <p class="text-4xl">seconds</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </template>
-  </VueCountdown>
-</section>
+  <div class="h-full bg-cover neon-grey-2" :style="{ backgroundImage: `url(${bgImage})` }" id="outer-container">
+    <div class="md:h-screen">
+      <section class="flex justify-center">
+        <img class="sm:w-2/3" src="../assets/assets24/magpAI_logo.png">
+      </section>
+      <section class="mt-4 mb-8">
+        <TimerComponent />
+      </section>
+    </div>
     <div class="w-10/12 mx-auto">
       <div class="grid gap-4 md:grid-cols-5 grid-cols-1">
         <div class="relative">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>Aberrant Watchdogs (OSINT)</strong>A group that monitor and act on aberrant reports or occurrences to prevent them from causing chaos.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>Aberrant Watchdogs (OSINT)</strong>A group that monitor and act on aberrant reports or occurrences to
+            prevent them from causing chaos.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/AberrantWatchdogs.webp">
         </div>
         <div class="relative flex">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>BinCortex (Binary)</strong> A development group responsible for most software and APIs used by modern androids you might see for sale or on the street.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>BinCortex (Binary)</strong> A development group responsible for most software and APIs used by modern
+            androids you might see for sale or on the street.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/BinCortex.webp">
         </div>
         <div class="relative flex">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>QuantumLocks (Crypto)</strong>A group who provide and research AI security (including quantum) solutions.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>QuantumLocks (Crypto)</strong>A group who provide and research AI security (including quantum)
+            solutions.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/QuantumLocks.webp">
         </div>
         <div class="relative flex">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>MagpAI (Web)</strong>A group focused on creating web assistants and can be found built into every mainstream internet browser.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>MagpAI (Web)</strong>A group focused on creating web assistants and can be found built into every
+            mainstream internet browser.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/magpai.webp">
         </div>
         <div class="relative flex">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>REAssembly (RevEng)</strong>A maintenance group who provide services that focus primarily on revamping and patching outdated and deprecated androids and AIs.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>REAssembly (RevEng)</strong>A maintenance group who provide services that focus primarily on revamping
+            and patching outdated and deprecated androids and AIs.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/REAssembly.webp">
         </div>
         <div class="relative flex">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>Rogueware (Forensics)</strong> A hacker group that advocate against the use of androids and AI by creating malware and attacking AI-related organizations.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>Rogueware (Forensics)</strong> A hacker group that advocate against the use of androids and AI by
+            creating malware and attacking AI-related organizations.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/Rogueware.webp">
         </div>
-        <div class="relative col-span-3 rounded-lg text-white text-center">
-          <p class="absolute text-sm 2xl:text-md bg-black m-4 2xl:m-12">These days, most of the business in the modern world is dominated by the Big 7. Each of these groups allow anyone to complete tasks for them in return for rewards in the form of “bounties.” A web service known colloquially as <str>“The Board”</str> has bounty postings from various users and organizations (anonymous and self-identified) where those interested in making some side-cash can take up these challenges and turn them in. You are one such user, and with The Game coming up, your goal is to complete as many such bounties and reach the top of the leaderboard. <br><br> The year is 2045. Breakthroughs in AI and quantum computing have allowed for androids and AI integration into modern society. Androids have been used to serve humans and complete the mundane tasks that nobody wants to do, allowing humanity to focus on the more fulfilling tasks of life. However, the complexity and true potential of the technology has surpassed our understanding of its mechanics, and the androids sometimes behave in… unexpected manners. These “aberrants” have a variety of potential unorthodox behaviors, ranging from difficulty understanding and completing tasks to outright violence and aggression.</p>
+        <div class="relative col-span-3 text-white text-center">
+          <p class="p-8 text-sm 2xl:text-md rounded-lg bg-black m-4 2xl:m-12"> The
+            year is 2045. Breakthroughs in AI and quantum computing have allowed for androids and AI integration into
+            modern society. Androids have been used to serve humans and complete the mundane tasks that nobody wants to
+            do, allowing humanity to focus on the more fulfilling tasks of life. However, the complexity and true
+            potential of the technology has surpassed our understanding of its mechanics, and the androids sometimes
+            behave in… unexpected manners. These “aberrants” have a variety of potential unorthodox behaviors, ranging
+            from difficulty understanding and completing tasks to outright violence and aggression. <br><br>These days, most of the business in the
+            modern world is dominated by the Big 7. Each of these groups allow anyone to complete tasks for them in return
+            for rewards in the form of “bounties.” A web service known colloquially as <strong>The Board</strong> has
+            bounty postings from various users and organizations (anonymous and self-identified) where those interested in
+            making some side-cash can take up these challenges and turn them in. You are one such user, and with The Game
+            coming up, your goal is to complete as many such bounties and reach the top of the leaderboard.</p>
         </div>
         <div class="relative flex">
-          <div class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2"><strong>Singularity (Networks)</strong>An advocate group calling for AI to be freed from humanity’s chains and allowed to live and co-exist among people as equals.</div>
+          <div
+            class="absolute flex-col flex opacity-0 hover:opacity-100 duration-300 top-0 left-0 px-5 rounded-lg h-full justify-center items-center text-center bg-black text-white text-sm 2xl:text-md bg-opacity-75 p-2">
+            <strong>Singularity (Networks)</strong>An advocate group calling for AI to be freed from humanity’s chains and
+            allowed to live and co-exist among people as equals.
+          </div>
           <img class="rounded-lg h-auto" src="../assets/assets24/Singularity.webp">
         </div>
       </div>
     </div>
-    <section class="w-full">
-      <p class="font-semibold text-center text-xl sm:text-5xl pt-4 pb-64">
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="neon-grey-2 underline" target="_blank">MagpAI CTF 2024</a>
+    <section>
+      <p class="font-semibold text-center text-xl sm:text-6xl pt-4 my-10">
+        <a href="https://2024.magpiectf.ca/" class="neon-grey-2 bg-black p-8 pt-4 rounded-lg underline" target="_blank">Register for MagpAI CTF 2024</a>
       </p>
     </section>
+    <AboutComponent />
     <FooterComponent />
   </div>
 </template>
 
 <script>
-import VueCountdown from '@chenfengyuan/vue-countdown'
+import AboutComponent from '../components/AboutComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue'
+import TimerComponent from '../components/TimerComponent.vue'
 
 export default {
   name: 'HomeComponent',
   components: {
-    VueCountdown,
-    FooterComponent
+    AboutComponent,
+    FooterComponent,
+    TimerComponent
   },
   data () {
     return {
